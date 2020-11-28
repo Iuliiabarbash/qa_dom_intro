21aae8522787e99fb67043fa878a652b8401fb4c
AJ Larson <aj@devmounta.in>
2020-11-28 14:46:20 -0700

    adding 2.04 practice solution

--- src/App.js
+++ src/App.js
@@ -13,7 +13,11 @@ function App() {
       </header>
       <main>
         <div>Hello world!</div>
-        <div>Other stuff</div>
+        <div>
+          Other stuff
+          {/* Here is where we added the extra block of text in a paragraph tag */}
+          <p> I like counting. </p>
+        </div>
         <Counter /> {/* React knows to use the Counter component here. */}
       </main>
       <footer>Goodbye</footer>
@@ -29,6 +33,7 @@ class Counter extends Component {
     // the number is stored in the component here
     this.state = { myNumber: 0 };
     this.incrementNumber = this.incrementNumber.bind(this);
+    this.decrementNumber = this.decrementNumber.bind(this);
   }
 
   // this function updates the number in the state
@@ -40,12 +45,24 @@ class Counter extends Component {
     });
   }
 
+  // this new function will reduce `myNumber` by 1
+  decrementNumber() {
+    console.log(`Removing 1 from ${this.state.myNumber}`);
+    this.setState((state) => {
+      return { myNumber: state.myNumber - 1 };
+    });
+  }
+
   render() {
     // this displays the current number AND tells the button to use the incrementNumber function()
     return (
-      <div>
+      <div id='numberCounter'>
+        {" "}
+        {/*We added the id here*/}
         <h4> The number is: {this.state.myNumber}</h4>
         <button onClick={this.incrementNumber}>Add 1</button>
+        <button onClick={this.decrementNumber}>Subtract 1</button>
+        {/* now it also instructs the component to render the "Subtract 1" button, which uses the function decrementNumber */}
       </div>
     );
   }
